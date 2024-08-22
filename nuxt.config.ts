ad36d87af8374f4a7f6eed6cfd53bbe6c441397a
beanmommo <leonardo.prasetyo5@gmail.com>
2024-08-22 11:58:31 +1000

    added prime vue components

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,24 @@
+import Aura from "@primevue/themes/aura";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  css: ["@/assets/main.scss"],
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
           additionalData: '@import "@/assets/global.scss"; ',
         },
+      },
+    },
+  },
+  modules: ["@primevue/nuxt-module"],
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura,
       },
     },
   },
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,26 @@
+<script setup lang="ts">
+const ingredient = ref('')
+</script>
 <template>
     <div>
-        Halo
+        <Button label="Check" icon="pi pi-check" />
+    </div>
+    <div class="flex items-center">
+        <RadioButton v-model="ingredient" inputId="ingredient1" name="pizza" value="Cheese" />
+        <label for="ingredient1" class="ml-2">Cheese</label>
+    </div>
+    <div class="flex items-center">
+        <RadioButton v-model="ingredient" inputId="ingredient2" name="pizza" value="Mushroom" />
+        <label for="ingredient2" class="ml-2">Mushroom</label>
+    </div>
+    <div class="flex items-center">
+        <RadioButton v-model="ingredient" inputId="ingredient3" name="pizza" value="Pepper" />
+        <label for="ingredient3" class="ml-2">Pepper</label>
+    </div>
+    <div class="flex items-center">
+        <RadioButton v-model="ingredient" inputId="ingredient4" name="pizza" value="Onion" />
+        <label for="ingredient4" class="ml-2">Onion</label>
     </div>
 </template>
 
-<style lang="scss" scoped>
-div {
-    font-size: $test_variable;
-}
-</style>
+<style lang="scss" scoped></style>
